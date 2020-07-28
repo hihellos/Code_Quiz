f21f2a4f178b3088a709f4e1ce266767e8ebaddd
hihello <13111181+hihellos@users.noreply.github.com>
2020-07-28 07:36:50 -0400

    working on countdown from 90

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ $(document).ready(function() {
     var correct = 0;
     var wrong = 0;
 
-    
 
 
+// Countdown Timer
+    $("#startTime").on("click", function() {
+        var counter = 90;
+        setInterval(function() {
+            counter--;
+            if (counter >= 0) {
+                span = document.getElementById("count");
+                span.innerHTML = counter;
+            }
+            if (counter === 0) {
+                alert("Quiz(time) is over");
+                clearInterval(counter);
+            }
+        }, 1000);
+        console.log(counter)
+    });
 })
