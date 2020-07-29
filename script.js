22e7a6bec71b3b18891c3e258c9e37ef19b7c15c
hihello <13111181+hihellos@users.noreply.github.com>
2020-07-29 19:33:03 -0400

    almost starting over, kept questionArray with question objects, index page correctly displays welcome, timer & questions hidden

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+// $(document).ready(function() {
 // Game Questions
     var gameQuestions = [
         {
@@ -62,7 +62,27 @@ $(document).ready(function() {
             answer: "Monica"
         }, 
     ];
-    
+
+    // GIVEN I am taking a code quiz
+    // WHEN I click the start button
+    $("#start-time").on("click", function() {
+        alert("I've been clicked!");
+    });
+    // THEN a timer starts and I am presented with a question
+    // WHEN I answer a question
+    // THEN I am presented with another question
+    // WHEN I answer a question incorrectly
+    // THEN time is subtracted from the clock
+    // WHEN all questions are answered or the timer reaches 0
+    // THEN the game is over
+    // WHEN the game is over
+    // THEN I can save my initials and score
+
+    // Game Buttons
+    // var startButton = 
+    // var QuestionContainer = 
+    // var question
+    // var answerChoices 
     
  // Countdown Timer
    
@@ -85,7 +105,7 @@ $(document).ready(function() {
         console.log(counter)
     });
 
-// Onclick hide welcome message, show first question
 
 
-})
+
+// });
